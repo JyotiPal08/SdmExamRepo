fde128247aa8711897d0524a4213e9e3780fb6c9
jyoti1pal1 <117821427+jyoti1pal1@users.noreply.github.com>
2023-02-02 09:07:26 +0530

    commited changes

---
+++ src/GitOperations/Main.java
@@ -0,0 +1,14 @@
+package GitOperations;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println(checkIfInputAnEvenNumber(122));
+
+	}
+
+	public static boolean checkIfInputAnEvenNumber(int number) {
+		return number%2==0;
+	}
+
+}
